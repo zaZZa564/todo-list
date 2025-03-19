d83a65caf87398da000f334b3e6a5e8f6bad6ad0
zaZZa564 <anus_bobra564@mail.ru>
2025-03-19 07:49:02 +0300

    placeholder

--- src/components/sections/Section-1.js
+++ src/components/sections/Section-1.js
@@ -8,7 +8,7 @@ class Section1 extends React.Component {
           <h1>Header of the section 1 text</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non sagittis nunc. Nullam faucibus enim vel magna eleifend dictum. Aenean malesuada fermentum nulla at finibus. Sed laoreet neque lorem. Sed et finibus nisl, at pharetra eros. Proin lorem urna, congue ut laoreet vitae, condimentum nec felis. Integer ac posuere libero. Etiam euismod semper iaculis. Curabitur volutpat ullamcorper leo nec fringilla.</p>
         </div>
-        <div className="sec1-img"></div>
+        <div className="sec1-img">placeholder</div>
       </div>
     );
   }
